028a363ca12fb613a0f5a7da69bed21b5f91be36
Pirmin Fix <pirmin.fix@jimdo.com>
2014-01-13 21:08:19 +0100

    random!

---
+++ bla.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/go-gl/gl"
+	glfw "github.com/go-gl/glfw3"
+	"log"
+)
+
+func main() {
+
+	maj, min, _ := glfw.GetVersion()
+	if maj != 3 || min != 1 {
+		log.Panicf("OpenGL: %v.%v\n", maj, min)
+	}
+	glfw.Init()
+	window, err := glfw.CreateWindow(1024, 768, "Testing", nil, nil)
+	if err != nil {
+		log.Fatal("Failed to open GLFW window: " + err.Error())
+	}
+
+	window.MakeContextCurrent()
+	if gl.Init() != 0 {
+		log.Fatal("Failed to init GL")
+	}
+
+	gl.ClearColor(0.0, 0.0, 0.3, 0.0)
+
+	// create vertexbuffer
+	gVertexBufferData := []float32{-1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 0.0}
+	vertexBuffer := gl.GenBuffer()
+	vertexBuffer.Bind(gl.ARRAY_BUFFER)
+	gl.BufferData(gl.ARRAY_BUFFER, len(gVertexBufferData)*4, gVertexBufferData, gl.STATIC_DRAW)
+
+	for {
+		// clear screen
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		// first attribute buffer: vertices
+		var vertexAttrib gl.AttribLocation = 0
+		vertexAttrib.EnableArray()
+		vertexBuffer.Bind(gl.ARRAY_BUFFER)
+		vertexAttrib.AttribPointer(
+			3, // size
+			gl.FLOAT,
+			false, // normalized?
+			0,     // stride
+			nil)   // array buffer offset
+
+		// draw the triangle
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+		vertexAttrib.DisableArray()
+
+		window.SwapBuffers()
+	}
+}
