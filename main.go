b985c451b57c5fbcce2504993f2351f29d559094
Pirmin Fix <pirmin.fix@jimdo.com>
2014-02-28 16:26:01 +0100

    Window handling done right this time
    no rendering though. And we're using a makefile now.

--- main.go
+++ main.go
@@ -1,38 +1,48 @@
 package main
 
 import (
-	"github.com/go-gl/gl"
-	glfw "github.com/go-gl/glfw3"
 	"log"
+
+	"github.com/go-gl/glfw3"
+	//"github.com/niemeyer/qml/gl"
 )
 
+const NAME = "simplegl"
+const VERSION = "0.0.1"
+
+// callback handler for glfw errors
+func glfwErrorCallback(error glfw3.ErrorCode, description string) {
+	log.Fatalf("glfw error: %v: %v\n", error, description)
+}
+
+// callback handler for glfw key events
+func glfwKeyCallback(window *glfw3.Window, key glfw3.Key, scancode int, action glfw3.Action, mods glfw3.ModifierKey) {
+	if key == glfw3.KeyEscape && action == glfw3.Press {
+		window.SetShouldClose(true)
+	}
+}
+
 func main() {
-	glfw.Init()
-	window, err := glfw.CreateWindow(
-		1024,       // width
-		768,        // height
-		"simplegl", // title
-		nil,        // monitor
-		nil,        // window to share context with
-	)
+	// First step: We need a window with opengl context
+	if !glfw3.Init() {
+		log.Fatal("glfw initialization failed")
+	}
+	defer glfw3.Terminate()
+	glfw3.SetErrorCallback(glfwErrorCallback)
+
+	window, err := glfw3.CreateWindow(1024, 768, NAME, nil, nil)
 	if err != nil {
-		log.Fatalf("Failed to open GLFW window: %v\n", err.Error())
+		log.Fatalf("CreateWindow failed: %v\n", err)
 	}
 	defer window.Destroy()
-
-	glInitErr := gl.Init()
-	if glInitErr != 0 {
-		log.Fatalf("Failed to init GL: %v\n", glInitErr)
-	}
 	window.MakeContextCurrent()
-	defer glfw.Terminate()
-
-	// TODO
-	//shader := shaderProgram(window)
-	//shader.Link()
-	//shader.Use()
+	window.SetKeyCallback(glfwKeyCallback)
 
 	for !window.ShouldClose() {
-		// TODO
+		// Might be used as a timer or something
+		// leaving this here as a reminder of its existence
+		//time := glfw3.GetTime()
+		glfw3.PollEvents()
+		window.SwapBuffers()
 	}
 }
