a828c7103438e870c31860eda3aeb77c550f6d2c
Pirmin Fix <pirmin.fix@jimdo.com>
2014-03-02 22:26:16 +0100

    Adding a prototype of a png2texture reader

---
+++ png2array.go
@@ -0,0 +1,38 @@
+// convert png's to 2d texture arrays for opengl
+package main
+
+import (
+	"image"
+	"log"
+	"os"
+)
+import _ "image/png"
+
+func img2array(img image.Image) []uint32 {
+	rect := img.Bounds()
+	spanX := rect.Max.X - rect.Min.X
+	spanY := rect.Max.Y - rect.Min.Y
+	imgSlice := make([]uint32, spanX*spanY)
+	for x := rect.Min.X; x < rect.Max.X; x++ {
+		for y := rect.Min.Y; y < rect.Max.Y; y++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			imgSlice = append(imgSlice, r, g, b, a)
+		}
+	}
+	return imgSlice
+}
+
+func png2array(filename string) []uint32 {
+	imgFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Loading image file failed: %v\n", err)
+	}
+	img, format, err := image.Decode(imgFile)
+	if err != nil {
+		log.Fatalf("Decoding image file failed: %v\n", err)
+	}
+	if format != "png" {
+		log.Printf("Strangely, format was not png but %v!\n", format)
+	}
+	return img2array(img)
+}
