e919ac092b3e8f27e0649618a0aaea7a30c7350a
Pirmin Fix <pirmin.fix@jimdo.com>
2014-03-08 08:40:25 +0100

    make animation between dog and cat

--- fragment.glsl
+++ fragment.glsl
@@ -7,9 +7,11 @@ out vec4 outColor;
 uniform sampler2D texCat;
 uniform sampler2D texDog;
 
+uniform float time;
+
 void main() {
 
 	vec4 colCat = texture(texCat, Texcoord);
 	vec4 colDog = texture(texDog, Texcoord);
-	outColor = mix(colDog, colCat, 0.5);
+	outColor = mix(colDog, colCat, sin(time));
 }
